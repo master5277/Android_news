1897ab7a5e5a8036a69562f2b62f7a92dc000641
2324 <2324139312@qq.com>
2024-04-16 16:16:21 +0800

    第一次提交

---
+++ app/src/main/java/com/app/news/MainActivity.java
@@ -0,0 +1,32 @@
+package com.app.news;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.news.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private AppBarConfiguration mAppBarConfiguration;
+//    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
