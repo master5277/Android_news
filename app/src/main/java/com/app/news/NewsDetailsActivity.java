89dac18ca5d78f31f978db3c24a85d3a22f82880
2324 <2324139312@qq.com>
2024-04-17 22:45:11 +0800

    第六次提交:看完第六集

--- app/src/main/java/com/app/news/NewsDetailsActivity.java
+++ app/src/main/java/com/app/news/NewsDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.app.news;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
@@ -23,8 +24,25 @@ public class NewsDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news_details);
 
 
+        toolbar = findViewById(R.id.toolbar);
+        mWebView = findViewById(R.id.webView);
+
         //获取传递的数据
         dataBean = (NewsInfo.ResultBean.DataBean) getIntent().getSerializableExtra("dataDTO");
 
+        //设置数据
+        if (null!=dataBean)
+        {
+            toolbar.setTitle(dataBean.getTitle());
+            mWebView.loadUrl(dataBean.getUrl());
+        }
+
+        //返回
+        toolbar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
