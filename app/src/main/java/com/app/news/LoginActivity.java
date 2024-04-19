335f932db92311251122200d6a44c5f0c3cbbb3e
2324 <2324139312@qq.com>
2024-04-19 22:34:51 +0800

    第九次提交:第二集登录注册(sp)完成

--- app/src/main/java/com/app/news/LoginActivity.java
+++ app/src/main/java/com/app/news/LoginActivity.java
@@ -1,6 +1,14 @@
 package com.app.news;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +18,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView register ;
+    private Button login;
+    private EditText et_username ;
+    private EditText et_password;
+    private SharedPreferences mSharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+        mSharedPreferences = getSharedPreferences("user",MODE_PRIVATE);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转注册页面
+                Intent  intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = et_username.getText().toString();
+                String password = et_password.getText().toString();
+                if (TextUtils.isEmpty(username)&&TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(LoginActivity.this, "请输入登录信息", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    String name = mSharedPreferences.getString("username",null);
+                    String pwd = mSharedPreferences.getString("password",null);
+                    if (username.equals(name)&&password.equals(pwd))
+                    {
+                        //登陆成功
+                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }else
+                    {
+                        Toast.makeText(LoginActivity.this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/app/news/RegisterActivity.java
+++ app/src/main/java/com/app/news/RegisterActivity.java
@@ -1,6 +1,12 @@
 package com.app.news;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +16,51 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button register;
+    private EditText et_username;
+    private EditText et_password;
+
+    private SharedPreferences mSharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        register = findViewById(R.id.register);
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+
+        //获取SharedPreferences
+        mSharedPreferences = getSharedPreferences("user",MODE_PRIVATE);
+
+        findViewById(R.id.toolbar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    //不要跳转
+                finish();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = et_username.getText().toString();
+                String password = et_password.getText().toString();
+                if (TextUtils.isEmpty(username)&&TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(RegisterActivity.this, "请输入用户名和密码", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    SharedPreferences.Editor edit = mSharedPreferences.edit();
+                    edit.putString("username",username);
+                    edit.putString("password",password);
+
+                    //一定要提交
+                    edit.commit();
+                    Toast.makeText(RegisterActivity.this, "注册成功，请登录", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+
     }
 }
